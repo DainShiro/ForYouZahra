c13d901795eb7b6022ab6f8a71ee397ee5b9affa
DainShiro <162948926+DainShiro@users.noreply.github.com>
2026-02-14 10:09:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -42,14 +42,14 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "WOOOOW You love me that much?? ",  // Shows when they go past 5000%
+        high: "To infinity and beyond! ",              // Shows when they go past 1000%
+        normal: "And beyond! "                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
+        title: "Yay! I'm the luckiest person in the world! 🎉",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
         emojis: "🎁"  // These will bounce around
     },
